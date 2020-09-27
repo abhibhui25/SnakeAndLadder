7b0df7a60b8fad106bb3de1d732630e6244bc3ec
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-27 20:25:07 +0530

    repeating the roll of dice till player reaches 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,8 @@ public class SnakeAndLadder {
 	public static void main(String args[]) {
 		int p1 = 0;
 		System.out.println("Welcome to snake and ladder game");
-		System.out.println("The player is at position " + p1);
+		
+		while(p1<100){ 
 		int i = (int) (Math.random() * 6 % 6) + 1;
 		System.out.println("value on the dice: " + i);
 		int o = (int) (Math.random() * 10 % 3);
@@ -30,5 +31,7 @@ public class SnakeAndLadder {
 		default:
 			System.out.println("No Movement");
 		}
+		}
+		
 	}
 }
