9e13399699e3966ae1386795978c0c575bea9f1f
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-27 19:52:44 +0530

    start the game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,8 @@
 
 public class SnakeAndLadder {
 	public static void main(String args[]) {
-
+		int p1=0;
 		System.out.println("Welcome to snake and ladder game");
-
+		System.out.println("The player is at position "+p1);
 	}
 }
