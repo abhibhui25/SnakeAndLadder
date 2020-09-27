4f8a01dd62d4ccb37da9e98eb92603a0a8e94221
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-27 19:59:37 +0530

    rolling the die is added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
 		int p1=0;
 		System.out.println("Welcome to snake and ladder game");
 		System.out.println("The player is at position "+p1);
+		int i=(int)(Math.random()*6%6)+1;
+		System.out.println("value on the dice: "+i);
 	}
 }
