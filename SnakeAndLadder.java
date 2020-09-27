dc590cecc5d483c37ffbc6c5101ec75246590d2d
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-09-27 20:14:32 +0530

    ladder snake and no movement is decided

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,34 @@
 
 public class SnakeAndLadder {
 	public static void main(String args[]) {
-		int p1=0;
+		int p1 = 0;
 		System.out.println("Welcome to snake and ladder game");
-		System.out.println("The player is at position "+p1);
-		int i=(int)(Math.random()*6%6)+1;
-		System.out.println("value on the dice: "+i);
+		System.out.println("The player is at position " + p1);
+		int i = (int) (Math.random() * 6 % 6) + 1;
+		System.out.println("value on the dice: " + i);
+		int o = (int) (Math.random() * 10 % 3);
+
+		System.out.println("option: " + o);
+		switch (o) {
+		case 1: {
+			System.out.println("Ladder");
+			p1 += i;
+			System.out.println("The new position of the player is: "+p1);
+			break;
+		}
+		case 2: {
+			System.out.println("Snake");
+			if (p1 - i > -1) {
+				p1 -= i;
+				System.out.println("The new position of the player is: " + p1);
+			} else {
+				p1 = 0;
+				System.out.println("The new position of the player is: " + p1);
+			}
+			break;
+		}
+		default:
+			System.out.println("No Movement");
+		}
 	}
 }
